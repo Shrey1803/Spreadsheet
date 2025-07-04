39ba3bd71dc726f3274b830f6cc8dc52a9940ed3
shrey <shreygarg1803@gmail.com>
2025-07-04 22:08:15 +0530

    Add initial mock data for job requests

---
+++ src/data/mockData.ts
@@ -0,0 +1,57 @@
+export const mockData = [
+  {
+    jobRequest: "Launch social media campaign",
+    submitted: "15-11-2024",
+    status: "In progress",
+    submitter: "Aisha Patel",
+    url: "www.aishapatel.com",
+    assigned: "Sophie Choudhary",
+    priority: "Medium",
+    dueDate: "20-11-2024",
+    value: "6,200,000 $"
+  },
+  {
+    jobRequest: "Update press kit for redesign",
+    submitted: "28-10-2024",
+    status: "Need to start",
+    submitter: "Irfan Khan",
+    url: "www.irfankhan.com",
+    assigned: "Tejas Pandey",
+    priority: "High",
+    dueDate: "30-10-2024",
+    value: "3,500,000 $"
+  },
+  {
+    jobRequest: "Finalize user testing report",
+    submitted: "05-12-2024",
+    status: "In progress",
+    submitter: "Mark Johnson",
+    url: "www.researchlog.in",
+    assigned: "Rachit Lee",
+    priority: "Medium",
+    dueDate: "12-12-2024",
+    value: "4,750,000 $"
+  },
+  {
+    jobRequest: "Design new features for website",
+    submitted: "10-01-2025",
+    status: "Complete",
+    submitter: "Emily Green",
+    url: "www.uiuxintern.io",
+    assigned: "Tom Wright",
+    priority: "Low",
+    dueDate: "15-01-2025",
+    value: "6,900,000 $"
+  },
+  {
+    jobRequest: "Prepare financial report for Q4",
+    submitted: "25-01-2025",
+    status: "Blocked",
+    submitter: "Jessica Brown",
+    url: "www.fiskalbank.in",
+    assigned: "Kevin Smith",
+    priority: "Low",
+    dueDate: "30-01-2025",
+    value: "2,800,000 $"
+  }
+];
